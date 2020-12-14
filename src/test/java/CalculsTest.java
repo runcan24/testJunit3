9692496f1bb9de29d791b767c696bbcb3783ad0c
Yann <yannther99@gmail.com>
2020-12-14 16:40:32 +0100

    maj

--- src/test/java/CalculsTest.java
+++ src/test/java/CalculsTest.java
@@ -1,12 +1,13 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class CalculsTest {
 
     @Test
     public void testerMultipliser(){
         Calculs calculs = new Calculs(2,4);
+
         int valMult = calculs.multiplier();
 
         assertEquals(valMult, 8);
@@ -15,6 +16,7 @@ public class CalculsTest {
     @Test
     public void testerAdditionner(){
         Calculs calculs = new Calculs(2,4);
+
         int valAdd = calculs.additionner();
 
         assertEquals(valAdd, 6);
