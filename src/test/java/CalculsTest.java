ef28773a6e8dad9fb0e362964b1889fca64e1251
Yann <yannther99@gmail.com>
2020-12-14 14:52:32 +0100

    first commit

---
+++ src/test/java/CalculsTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculsTest {
+    /**
+     * Initialise les valeurs avant chaque test
+     */
+    @BeforeEach
+    void
+
+    /**
+     * Test method for {@link Calculs#multiplier()}.
+     */
+    @Test
+    public void multiplier() {
+
+    }
+}
