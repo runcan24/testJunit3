9d9e1fc7c727b2ed79b86df604879368f0f07f2c
Yann <yannther99@gmail.com>
2020-12-14 15:14:40 +0100

    maj

--- src/test/java/CalculsTest.java
+++ src/test/java/CalculsTest.java
@@ -1,18 +1,27 @@
+import static org.junit.Assert.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculsTest {
-    /**
-     * Initialise les valeurs avant chaque test
-     */
+
+    private Calculs calculs = null;
+
     @BeforeEach
-    void
+    public void setUp(){
+        calculs = new Calculs(2,4);
+    }
 
-    /**
-     * Test method for {@link Calculs#multiplier()}.
-     */
     @Test
-    public void multiplier() {
+    public void testerMultipliser(){
+        int valMult = calculs.multiplier();
 
+        assertEquals(valMult, 8);
+    }
+
+    @Test
+    public void testerAdditionner(){
+        int valAdd = calculs.additionner();
+
+        assertEquals(valAdd, 6);
     }
 }
