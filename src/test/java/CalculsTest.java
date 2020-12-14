eafeaaeae394d52abbaefec6ec2791f39b92ef62
Yann <yannther99@gmail.com>
2020-12-14 15:46:24 +0100

    maj

--- src/test/java/CalculsTest.java
+++ src/test/java/CalculsTest.java
@@ -1,6 +1,7 @@
-import static org.junit.Assert.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculsTest {
 
